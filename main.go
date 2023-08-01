28a293eab49b97a0a73e89669fead8c073a740f0
odsource <christoph.kaiser93@gmail.com>
2023-08-01 12:28:30 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
+)
+
+var db *sql.DB
+
+type Album struct {
+	ID     int64
+	Title  string
+	Artist string
+	Price  float32
+}
+
+func indexHandler(c *fiber.Ctx, db *sql.DB) error {
+	return c.SendString("Hello")
+}
+
+func postHandler(c *fiber.Ctx, db *sql.DB) error {
+	return c.SendString("Hello")
+}
+
+func putHandler(c *fiber.Ctx, db *sql.DB) error {
+	return c.SendString("Hello")
+}
+
+func deleteHandler(c *fiber.Ctx, db *sql.DB) error {
+	return c.SendString("Hello")
+}
+
+func albumsByArtist(name string) ([]Album, error) {
+	// An albums slice to hold data from returned rows.
+	var albums []Album
+
+	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+	if err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	defer rows.Close()
+	// Loop through rows, using Scan to assign column data to struct fields.
+	for rows.Next() {
+		var alb Album
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		}
+		albums = append(albums, alb)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	}
+	return albums, nil
+}
+
+func dbHandler(c *fiber.Ctx, db *sql.DB) error {
+	albums, err := albumsByArtist("John Coltrane")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found: %v\n", albums)
+	return c.Render(
+		"index",
+		fiber.Map{
+			"Artist": albums[0].Artist,
+		},
+	)
+}
+
+func main() {
+	connStr := "postgresql://test_db_54z5_user:oZomOJY6lbM14PiTTBpj5FmExhFqWywK@dpg-cj40rpliuie55pnj6n9g-a/test_db_54z5"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("DB Connected!")
+
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return indexHandler(c, db)
+	})
+
+	app.Post("/", func(c *fiber.Ctx) error {
+		return postHandler(c, db)
+	})
+
+	app.Put("/update", func(c *fiber.Ctx) error {
+		return putHandler(c, db)
+	})
+
+	app.Delete("/delete", func(c *fiber.Ctx) error {
+		return deleteHandler(c, db)
+	})
+
+	app.Get("/testdb", func(c *fiber.Ctx) error {
+		return dbHandler(c, db)
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	app.Static("/", "./public")
+	log.Fatalln(app.Listen(fmt.Sprintf(":%v", port)))
+}
